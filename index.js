7bb69d906665c58c9a255b395b59783126fb3c1a
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2022-12-29 01:45:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ class UNEPOStack
     
     prettyPrint(value)
     {
-        console.log(JSON.stringify(value, null, 4));
+        const util = require('util');
+        console.log(util.inspect(value, { showHidden: true, colors: true, depth: 4 }));
     }
     
     confirmationMessage(error, data, message, line)
